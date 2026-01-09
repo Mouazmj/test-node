fb2b29c8b2a4f4b41fdea355dff6cf43cde26fdd
Mouaz Jamal <mj225ih@student.lnu.se>
2026-01-09 16:28:24 +0100

    There was a problem, get had to have be writen like that GET and remve write method bcs its not good with json

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const http = require('http')
 const tasks = require('./tasks')
 
 const server = http.createServer((req, res) => {
-if (req.url === '/tasks' && req.method === 'Get') {
+if (req.url === '/tasks' && req.method === 'GET') {
     res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write('Hello there!')
     res.end(JSON.stringify(tasks))
 } else {
     res.writeHead(404, { 'Content-Type': 'application/json' })
-    res.write('Something went wrong!')
     res.end(JSON.stringify({ message: 'Not Found' }))
 }
 })
--- tasks.js
+++ tasks.js
@@ -1,6 +1,6 @@
-const info = [
+const tasks = [
     {name: 'Mouaz', age: 20, hobby: 'coding'},
     {name: 'Jack', age: 22, hobby: 'basketball'},
     {name: 'Luca', age: 30, hobby: 'gym'}
 ]
- module.exports = info
+ module.exports = tasks
