7c0ddff67c08681a8533ec21f15fa283c59d3b4f
Mouaz Jamal <mj225ih@student.lnu.se>
2026-01-10 13:33:53 +0100

    Using fs ion the server to show the html code in the file

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const http = require('http')
 const tasks = require('./tasks')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
 if (req.url === '/tasks' && req.method === 'GET') {
-    res.writeHead(200, { 'Content-Type': 'text/html', 'Content-Type': 'application/json' });
-    res.write('<h1>Hej det är Mouaz</h1>')
-    res.end(JSON.stringify(tasks))
+    res.writeHead(200, { 'Content-Type': 'text/html'});
+
+    fs.readFile('./index.html', (err, data) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.end(data)
+        }
+    })
 } else {
-    res.writeHead(404, { 'Content-Type': 'text/html', 'Content-Type': 'application/json' })
-    res.write('<h1>Något gick fel!</h1>')
-    res.end(JSON.stringify({ message: 'Not found' }))
+    res.writeHead(404, { 'Content-Type': 'text/html'})
+    res.end()
 }
 })
 
