ded4d6fc07281c97740482cfd97f048e47605822
Mouaz Jamal <mj225ih@student.lnu.se>
2026-01-10 18:46:07 +0100

    Creating some html files so I can use them to my paths in the req url when the user use the search bar

--- server.js
+++ server.js
@@ -3,20 +3,7 @@ const tasks = require('./tasks')
 const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-if (req.url === '/tasks' && req.method === 'GET') {
-    res.writeHead(200, { 'Content-Type': 'text/html'});
 
-    fs.readFile('./index.html', (err, data) => {
-        if (err) {
-            console.log(err)
-        } else {
-            res.end(data)
-        }
-    })
-} else {
-    res.writeHead(404, { 'Content-Type': 'text/html'})
-    res.end()
-}
 })
 
 server.listen(3000, 'localhost', () => {
