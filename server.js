35ec6f6deb88036acc662a660a408af451ec78ef
Mouaz Jamal <mj225ih@student.lnu.se>
2026-01-10 18:53:46 +0100

    Usin the paths with switch to decide what file to shwo to what url path the user search

--- server.js
+++ server.js
@@ -3,7 +3,26 @@ const tasks = require('./tasks')
 const fs = require('fs')
 
 const server = http.createServer((req, res) => {
+let path = './views/'
+switch(req.url) {
+    case '/':
+    path += 'index.html';
+    break;
+    case '/about':
+    path += 'about.html';
+    break;
+    default:
+        path += '404.html'
+        break;
+}
 
+fs.readFile(path, (err, data) => {
+    if (err) {
+        console.log(err)
+    } else {
+        res.end(data)
+    }
+})
 })
 
 server.listen(3000, 'localhost', () => {
